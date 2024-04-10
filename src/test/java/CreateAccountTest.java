730d99ceb4a0dfcccae2a5f8a39ec667dec3cf0a
Chalova Anastasiia <nchalova93@gmail.com>
2024-04-10 12:19:51 +0200

    Updated

--- src/test/java/CreateAccountTest.java
+++ src/test/java/CreateAccountTest.java
@@ -13,6 +13,7 @@ public class CreateAccountTest extends TestBase {
                 type(By.name("Password"), "Chalova1@");
                 type(By.name("ConfirmPassword"), "Chalova1@");
                 click(By.cssSelector("[name='register-button']"));
+                //Assert computer in quantity
                 Assert.assertTrue(isElementFormPresent(By.cssSelector("[href='/logout']")));
 
         }
